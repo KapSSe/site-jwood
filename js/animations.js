78ed555beac9b76d522fcd386730551c6d4d059c
KapSSegit config --global user.email kapranovstanislav@gmail.comgit helpgit config --global user.name KapSSe <kapranovstanislav@gmail.com>
2017-07-01 21:01:21 +0300

    issues fixes

--- js/animations.js
+++ js/animations.js
@@ -1,6 +1,4 @@
 $(document).scroll(function(){
-    		 $('.section_heading').toggleClass('animated slideInRight', $(this).scrollTop() > 300);
-             $('.section_about_sign').toggleClass('animated slideInRight', $(this).scrollTop() > 300);
               $('.mail').toggleClass('animated fadeInLeft', $(this).scrollTop() > 2500);
               $('.mail_adress').toggleClass('animated fadeInRight', $(this).scrollTop() > 2500);
               $('.phone').toggleClass('animated fadeInLeft', $(this).scrollTop() > 2500);
