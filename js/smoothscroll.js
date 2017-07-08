12c50ff1e96ad3cd141aa7ba2bf7a13c0893586c
KapSSegit config --global user.email kapranovstanislav@gmail.comgit helpgit config --global user.name KapSSe <kapranovstanislav@gmail.com>
2017-07-08 12:18:30 +0300

    fixed links

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,6 +1,28 @@
-       $(document).on('click', 'a', function(event){
+    $('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
         event.preventDefault();
         $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-        }, 1000);
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {  
+            return false;
+          } else {
+            $target.attr('tabindex','-1');  
+            $target.focus();  
+          };
         });
+      }
+    }
+  });
